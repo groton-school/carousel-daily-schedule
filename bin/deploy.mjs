b43a72a8a1c746d3639a4a8e18134c19eee16bc7
Seth Battis <sbattis@groton.org>
2024-02-04 14:41:41 -0500

    update partly-cloudy, deploy

--- bin/deploy.mjs
+++ bin/deploy.mjs
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
 import gcloud from '@battis/partly-gcloudy';
+import cli from '@battis/qui-cli';
 
 await gcloud.init();
-await gcloud.app.deploy();
+try {
+  await gcloud.batch.appEngineDeployAndCleanup({ retainVersions: 2 });
+  cli.log.info('Deploy complete.');
+} catch (e) {
+  cli.log.error(e);
+}
